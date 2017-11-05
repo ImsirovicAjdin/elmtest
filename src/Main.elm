4f2011c96bc29956d7e2deed343b9dce01feffbf
ImsirovicAjdin <ajdika@live.com>
2017-11-05 09:00:37 +0100

    Note: It won't work without this update: main : Html msg main =     init.questions         |> List.map View.Question.view         |> String.join ", "         |> text

--- src/Main.elm
+++ src/Main.elm
@@ -2,7 +2,7 @@ module Main exposing (..)
 
 import Html exposing (Html, text)
 import Maybe
-import Util exposing (..)
+import Util exposing ((=>))
 import Data.Difficulty exposing (Difficulty, default)
 import Data.Question exposing (Question)
 import View.Question
@@ -12,33 +12,13 @@ type alias Model =
     , questions : List Question
     }
 
-unwrapMaybe : Maybe String -> String
-unwrapMaybe mb =
-    Maybe.withDefault "There's no answer" mb
-
- 
-parsedInt : Result String Int 
-parsedInt =
-    String.toInt "5"
-
-
-unwrapResult : Result String Int -> Int
-unwrapResult res =
-    case res of
-        Ok val -> 
-            val
-        Err err ->
-            5
-
-
-
 
 init : Model
 init = 
     Model
         default
         [ Question
-            (Just "TO get to the other side") -- Nothing
+            (Just "TO get to the other side")
             "Why did the chicken cross the road?"
             "To get to the other side"
             []
@@ -46,9 +26,7 @@ init =
 
 main : Html msg
 main =
-    -- view init
-    parsedInt
-        |> Result.toMaybe
-        |> Maybe.map toString
-        |> unwrapMaybe
+    init.questions
+        |> List.map View.Question.view
+        |> String.join ", "
         |> text
